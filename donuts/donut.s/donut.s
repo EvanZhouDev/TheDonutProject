14298a6304595bfcbe38753c691b6c34fb337243
Rue <78876133+IOKG04@users.noreply.github.com>
2023-10-20 20:27:01 +0000

    Its done ^w^ (kinda at least)

--- donuts/donut.s/donut.s
+++ donuts/donut.s/donut.s
@@ -29,7 +29,7 @@ movq %rsp, %rbp
                                    movq %rax, %rdi; movl $0, %eax; call printf@PLT; .L12:  leaq -1776(%rbp),%rax;
                                 movl $1760,%edx; movl $32,%esi;movq %rax,%rdi;call memset@PLT; leaq -8816(%rbp),%rax;
                               movl $7040, %edx;  movl $0, %esi;  movq %rax, %rdi;  call memset@PLT;  pxor %xmm0,%xmm0;
-                           movss %xmm0,-8880(%rbp); jmp .L2;. L7: pxor %xmm0,%xmm0; movss %xmm0,-8884(%rbp); jmp .L3;.L6:
+                           movss %xmm0,-8880(%rbp); jmp .L2; .L7: pxor %xmm0,%xmm0; movss %xmm0,-8884(%rbp); jmp .L3;.L6:
                          pxor %xmm3, %xmm3;  cvtss2sd -8884(%rbp), %xmm3; movq %xmm3, %rax; movq %rax,  %xmm0; call sin@PLT;
                       cvtsd2ss %xmm0,%xmm0;movss %xmm0,-8876(%rbp);pxor %xmm4,%xmm4;cvtss2sd -8880(%rbp),%xmm4;movq %xmm4,%rax;
                     movq  %rax,  %xmm0;  call cos@PLT;  cvtsd2ss %xmm0,  %xmm0;  movss %xmm0,  -8872(%rbp);  pxor  %xmm5,  %xmm5;
@@ -41,27 +41,16 @@ movq %rsp, %rbp
            mulss -8868(%rbp), %xmm1;  movss -8864(%rbp), %xmm0;  mulss -8860(%rbp),  %xmm0;  addss %xmm0, %xmm1;  movss .LC3(%rip),%xmm0;
           addss %xmm0,%xmm1;movss .LC4(%rip), %xmm0;divss %xmm1,%xmm0;movss %xmm0,-8852(%rbp);pxor %xmm3,%xmm3;cvtss2sd -8884(%rbp),%xmm3;
          movq  %xmm3,  %rax;  movq  %rax,  %xmm0;  call cos@PLT;  cvtsd2ss  %xmm0,  %xmm0;  movss  %xmm0,  -8848(%rbp);  pxor %xmm4,  %xmm4;
-cvtss2sd -8888(%rbp),%xmm4;movq %xmm4,%rax;movq %rax,%xmm0;call cos@PLT;cvtsd2ss %xmm0,%xmm0;movss %xmm0,-8844(%rbp);pxor %xmm5,%xmm5;
         cvtss2sd -8888(%rbp),%xmm4;movq %xmm4,%rax;movq %rax,%xmm0;call cos@PLT;cvtsd2ss %xmm0,%xmm0;movss %xmm0,-8844(%rbp);pxor %xmm5,%xmm5
-cvtss2sd -8888(%rbp),%xmm5;movq %xmm5,%rax;movq %rax,%xmm0;call sin@PLT;cvtsd2ss %xmm0,%xmm0;movss %xmm0,-8840(%rbp);
        cvtss2sd -8888(%rbp),  %xmm5;  movq %xmm5,  %rax;  movq %rax, %xmm0; call sin@PLT;  cvtsd2ss %xmm0,  %xmm0;  movss %xmm0,  -8840(%rbp);
-movss -8876(%rbp),%xmm0;mulss -8856(%rbp),%xmm0;mulss -8860(%rbp),%xmm0;movss -8864(%rbp),%xmm1;mulss -8868(%rbp),%xmm1;subss %xmm1,%xmm0;
       movss -8876(%rbp),%xmm0;mulss -8856(%rbp),%xmm0;mulss -8860(%rbp),%xmm0;movss -8864(%rbp),%xmm1;mulss -8868(%rbp),%xmm1;subss %xmm1,%xmm0
-movss %xmm0,-8836(%rbp);movss -8852(%rbp),%xmm1;movss .LC5(%rip),%xmm0;movaps %xmm1,%xmm2;mulss %xmm0,%xmm2;
      movss %xmm0,-8836(%rbp); movss -8852(%rbp),%xmm1;movss .LC5                     (%rip),  %xmm0; movaps %xmm1,  %xmm2;  mulss %xmm0,  %xmm2;
-movss -8848(%rbp),%xmm0;mulss -8856(%rbp),%xmm0;mulss -8844(%rbp),%xmm0;movss -8836(%rbp),%xmm1;
     movss  -8848(%rbp),  %xmm0;  mulss  -8856(%rbp),  %xmm0;                             mulss  -8844(%rbp),  %xmm0;  movss  -8836(%rbp),  %xmm1;
-mulss -8840(%rbp),%xmm1;subss %xmm1,%xmm0;mulss %xmm0,%xmm2;movaps %xmm2,%xmm1;movss .LC6(%rip),%xmm0;
    mulss -8840(%rbp),%xmm1;subss %xmm1,%xmm0;mulss %xmm0,                                   %xmm2;  movaps %xmm2, %xmm1; movss .LC6(%rip),  %xmm0;
-addss %xmm1,%xmm0;cvttss2sil %xmm0,%eax;movl %eax,-8832(%rbp);movss -8852(%rbp),%xmm1;
    addss %xmm1,  %xmm0;  cvttss2sil %xmm0, %eax;  movl                                         %eax,   -8832(%rbp);   movss  -8852(%rbp),   %xmm1;
-movss .LC7(%rip),%xmm0;mulss %xmm0,%xmm1;movss -8848(%rbp),%xmm0;mulss -8856(%rbp),%xmm0;
   movss .LC7(%rip), %xmm0; mulss %xmm0, %xmm1; movss                                             -8848(%rbp),  %xmm0;  mulss   -8856(%rbp),  %xmm0;
-movaps %xmm0,%xmm2;mulss -8840(%rbp),%xmm2;movss -8836(%rbp),%xmm0;mulss -8844(%rbp),%xmm0;
   movaps %xmm0,%xmm2; mulss -8840(%rbp),%xmm2;movss                                               -8836(%rbp),  %xmm0;  mulss  -8844(%rbp),  %xmm0;
-addss %xmm2,%xmm0;mulss %xmm0,%xmm1;movss .LC8(%rip),%xmm0;addss %xmm1,%xmm0;
  addss %xmm2,  %xmm0;  mulss %xmm0, %xmm1;  movss                                                   .LC8(%rip),    %xmm0;    addss  %xmm1,    %xmm0;
-cvttss2sil %xmm0,%eax;movl %eax,-8828(%rbp);movl -8828(%rbp),%edx;movl %edx,%eax; sall $2,%eax;addl %edx,%eax;sall $4,%eax;movl %eax,%edx;movl -8832(%rbp),%eax;addl %edx,%eax; movl %eax,-8824(%rbp);movss -8864(%rbp),%xmm0;mulss -8868(%rbp),%xmm0; movss -8876(%rbp),%xmm1;mulss -8872(%rbp),%xmm1;mulss -8860(%rbp),%xmm1; subss %xmm1,%xmm0;mulss -8844(%rbp),%xmm0;movss -8876(%rbp),%xmm1; mulss -8872(%rbp),%xmm1;mulss -8868(%rbp),%xmm1;subss %xmm1,%xmm0; movss -8864(%rbp),%xmm1;mulss -8860(%rbp),%xmm1;subss %xmm1,%xmm0; movss -8848(%rbp),%xmm1;mulss -8872(%rbp),%xmm1;movaps %xmm1,%xmm2; mulss -8840(%rbp),%xmm2;movaps %xmm0,%xmm1;subss %xmm2,%xmm1;movss .LC9(%rip),%xmm0; mulss %xmm1,%xmm0;cvttss2sil %xmm0,%eax;movl %eax,-8820(%rbp);cmpl $21,-8828(%rbp); jg .L4;cmpl $0,-8828(%rbp);jle .L4;cmpl $0,-8832(%rbp);jle .L4;cmpl $79,-8832(%rbp); jg .L4;movl -8824(%rbp),%eax;cltq;movss -8816(%rbp,%rax,4),%xmm1;movss -8852(%rbp),%xmm0; comiss %xmm1,%xmm0;jbe .L4;movl -8824(%rbp),%eax;cltq;movss -8852(%rbp),%xmm0; movss %xmm0,-8816(%rbp,%rax,4);movl -8820(%rbp),%eax;movl $0,%edx;testl %eax,%eax; cmovs %edx,%eax;cltq;leaq .LC10(%rip),%rdx;movzbl (%rax,%rdx),%edx;movl -8824(%rbp),%eax; cltq;movb %dl,-1776(%rbp,%rax);.L4:;pxor %xmm1,%xmm1;cvtss2sd -8884(%rbp),%xmm1; movsd .LC11(%rip),%xmm0;addsd %xmm1,%xmm0;cvtsd2ss %xmm0,%xmm0;movss %xmm0,-8884(%rbp);.L3: pxor %xmm1,%xmm1;cvtss2sd -8884(%rbp),%xmm1;movsd .LC12(%rip),%xmm0;comisd %xmm1,%xmm0;ja .L6; pxor %xmm1,%xmm1;cvtss2sd -8880(%rbp),%xmm1;movsd .LC13(%rip),%xmm0;addsd %xmm1,%xmm0; cvtsd2ss %xmm0,%xmm0;movss %xmm0,-8880(%rbp);.L2:;pxor %xmm1,%xmm1;cvtss2sd -8880(%rbp),%xmm1; movsd .LC12(%rip),%xmm0;comisd %xmm1,%xmm0;ja .L7;leaq .LC14(%rip),%rax;movq %rax,%rdi;movl $0,%eax; call printf@PLT;movl $0,k(%rip);jmp .L8;.L11:;movl k(%rip),%ecx;movslq %ecx,%rax;imulq $1717986919,%rax,%rax; shrq $32,%rax;movl %eax,%edx;sarl $5,%edx;movl %ecx,%eax;sarl $31,%eax;subl %eax,%edx;movl %edx,%eax;sall $2,%eax;addl %edx,%eax; sall $4,%eax;subl %eax,%ecx;movl %ecx,%edx;testl %edx,%edx;je .L9;movl k(%rip),%eax;cltq;movzbl -1776(%rbp,%rax),%eax;movsbl %al,%eax; jmp .L10;.L9:;movl $10,%eax;.L10:;movl %eax,%edi;call putchar@PLT;movl k(%rip),%eax;addl $1,%eax;movl %eax,k(%rip);.L8: movl k(%rip),%eax;cmpl $1760,%eax;jle .L11;pxor %xmm1,%xmm1;cvtss2sd -8892(%rbp),%xmm1;movsd .LC15(%rip),%xmm0;addsd %xmm1,%xmm0; cvtsd2ss %xmm0,%xmm0;movss %xmm0,-8892(%rbp);pxor %xmm1,%xmm1;cvtss2sd -8888(%rbp),%xmm1;movsd .LC11(%rip),%xmm0; addsd %xmm1,%xmm0;cvtsd2ss %xmm0,%xmm0;movss %xmm0,-8888(%rbp);jmp .L12
  cvttss2sil %xmm0, %eax; movl %eax, -8828(%rbp);                                                     movl  -8828(%rbp),   %edx;  movl   %edx,  %eax;
 sall $2,%eax; addl %edx,%eax; sall $4,%eax;movl                                                       %eax,%edx;movl -8832(%rbp),%eax;addl %edx,%eax;
 movl  %eax,  -8824(%rbp);  movss  -8864(%rbp),                                                         %xmm0;      mulss     -8868(%rbp),      %xmm0;
@@ -71,7 +60,7 @@ mulss -8872(%rbp), %xmm1;  mulss -8868(%rbp),                                   
 movss -8864(%rbp), %xmm1;  mulss -8860(%rbp),                                                           %xmm1;       subss        %xmm1,       %xmm0;
 movss -8848(%rbp), %xmm1;  mulss -8872(%rbp),                                                           %xmm1;       movaps       %xmm1,       %xmm2;
 mulss -8840(%rbp), %xmm2; movaps %xmm0,%xmm1;                                                           subss %xmm2, %xmm1;  movss .LC9(%rip), %xmm0;
-mulss %xmm1,%xmm0; cvttss2sil %xmm0,%eax;movl                                                           %eax,  -8820(%rbp);  cmpl $21,   -8828(%rbp);
+mulss %xmm1,%xmm0; cvttss2sil %xmm0,%eax;movl                                                           %eax,  -8820(%rbp);  cmpl $21,   -8828(%rbp); 
 jg .L4; cmpl $0,-8828(%rbp); jle .L4;cmpl $0,                                                           -8832(%rbp); jle .L4;  cmpl $79, -8832(%rbp);
 jg .L4;movl -8824(%rbp),%eax;cltq;movss -8816(                                                         %rbp,%rax,4), %xmm1; movss -8852(%rbp), %xmm0;
 comiss %xmm1,%xmm0; jbe .L4; movl -8824(%rbp),                                                         %eax;   cltq;   movss    -8852(%rbp),   %xmm0;
@@ -83,22 +72,22 @@ movss %xmm0,-8816(%rbp,%rax,4);movl -8820(%rbp)                                 
    pxor %xmm1,%xmm1; cvtss2sd -8880(%rbp), %xmm1;movsd                                         .LC13(%rip),    %xmm0;    addsd    %xmm1,    %xmm0;
    cvtsd2ss %xmm0,%xmm0;movss %xmm0,-8880(%rbp);.L2: pxor                                   %xmm1,    %xmm1;    cvtss2sd    -8880(%rbp),    %xmm1;
     movsd .LC12(%rip),%xmm0; comisd %xmm1,%xmm0;ja .L7; leaq                             .LC14(%rip),  %rax;  movq  %rax,  %rdi;  movl $0,  %eax;
-     call printf@PLT;movl $0,k(%rip);jmp .L8;.L11: movl k(%rip),                     %ecx;  movslq %ecx,  %rax; imulq $1717986919,  %rax,  %rax;
+     call printf@PLT;movl $0,k(%rip);jmp .L8;.L11:;movl k(%rip),                     %ecx;  movslq %ecx,  %rax; imulq $1717986919,  %rax,  %rax;
       shrq $32,%rax; movl %eax,%edx; sarl $5,%edx; movl %ecx,%eax; sarl $31,%eax; subl %eax,%edx; movl %edx,%eax; sall $2,%eax; addl %edx,%eax;
        sall $4,%eax;subl %eax,%ecx;movl %ecx,%edx;testl %edx,%edx;je .L9;movl k(%rip), %eax;cltq;movzbl -1776(%rbp,%rax),%eax;movsbl %al,%eax;
-        jmp .L10; .L9: movl $10, %eax; .L10:  movl %eax, %edi; call putchar@PLT; movl k(%rip), %eax; addl $1,  %eax; movl %eax, k(%rip); .L8:
+        jmp .L10; .L9: movl $10, %eax; .L10:  movl %eax, %edi; call putchar@PLT; movl k(%rip), %eax; addl $1,  %eax; movl %eax, k(%rip);.L8:
          movl k(%rip),%eax;cmpl $1760,%eax;jle .L11; pxor %xmm1,%xmm1;cvtss2sd -8892(%rbp),%xmm1; movsd .LC15(%rip),%xmm0;addsd %xmm1,%xmm0;
           cvtsd2ss %xmm0,  %xmm0; movss  %xmm0, -8892(%rbp);  pxor %xmm1,  %xmm1; cvtss2sd  -8888(%rbp), %xmm1;  movsd  .LC11(%rip), %xmm0;
-           addsd      %xmm1,      %xmm0;      cvtsd2ss      %xmm0,       %xmm0;      movss      %xmm0,      -8888(%rbp);      jmp      .L2
+           addsd      %xmm1,      %xmm0;      cvtsd2ss      %xmm0,       %xmm0;      movss      %xmm0,      -8888(%rbp);      jmp     .L12
             //...........................................................................................................................
               //.......................................................................................................................
                //.....................................................................................................................
                  //.................................................................................................................
                    //.............................................................................................................
-                    //...........................................................................................................
-                      //.......................................................................................................
-                         //.................................................................................................
-                           //.............................................................................................
+                    //.......................Sorry for all the blank space.......................................................
+                      //......................I might make a better version in the future......................................
+                         //....................Compile using `gcc -o donut donut.s -lc -lm`..................................
+                           //...................by Rue / @IOKG04 (Github).................................................
                               //.......................................................................................
                                 //...................................................................................
                                    //.............................................................................
