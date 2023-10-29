e4298a667cea97d7cf1f96ac859056f9c89e5f42
IOKG04 <iokg04@gmail.com>
2023-10-29 14:19:03 +0100

    String Parsing works
    Only the entirety of parsing everything else left (for this step...)
    (after that ill still have to make sure my way of pressing it into a donut still works)

--- tools/oneline2donut/Program.cs
+++ tools/oneline2donut/Program.cs
@@ -5,15 +5,22 @@ using System.Linq;
 
 /*
 
+TODO:
+ Parse strings
+ Make that thing with the formulars work
+ Press into donut shape
+ Test
+ pr :D
+
 c = coded
 r = reworking
 x = checked / done
 
 [r] Parse
  [x] Convert to primitives
+ [x] Detect strings
+  [x] Make escaping characters possible
  [ ] Merge primitives into tokens
- [ ] Detect strings
-  [ ] Make escaping characters possible
 [x] Donutify input
  [x] Generate donut template
  [x] Map input onto template without splitting groups
@@ -28,7 +35,7 @@ Errors marked with `(I)` are internal. Please open an issue and describe what ex
 namespace oneline2donut;
 
 public class Program{
-    public static Primitive[]	primitives;
+    public static List<Token>	tokens;
     public static int		totalCharCount;
     public static string[]	donutTemplate;
     public static string	outp;
@@ -38,9 +45,12 @@ public class Program{
 
 	string baseCode = File.ReadAllText(args[0]);
 
-	primitives = Primitive.FindPrimitives(baseCode);
+	tokens = Token.FindPrimitives(baseCode);
 	Console.WriteLine(baseCode);
-	foreach(Primitive p in primitives) Console.WriteLine(p.ToString(true));
+	foreach(Token t in tokens) Console.WriteLine(t.ToString(true));
+	Console.WriteLine("\nPARSING STRINGS\n");
+	Token.ParseStrings(ref tokens);
+	foreach(Token t in tokens) Console.WriteLine(t.ToString(true));
 
 	/*
 	// filter baseCode
@@ -165,17 +175,16 @@ _generate_donut:
     }
 }
 
-// primitives are single letters (or rarely multiple letters) that serve a specific function
-// different kinds of primitives are assumed to be combinable in specific ways to create tokens
-public struct Primitive{
-    public PrimitiveGroup	primitiveGroup;
-    public string		content;
+// tokens are combinations of Primitives that have a specific function in a programming language, like a function name, a discriminator, etc.
+public struct Token{
+    public TokenGroup	tokenGroup;
+    public string	content;
     public string ToString(bool showGroup = false){
-	string outp = showGroup ? "{ \"group\": \"" + primitiveGroup.ToString() + "\", \"content\": \"" + content + "\" }" : content;
+	string outp = showGroup ? "{ \"group\": \"" + tokenGroup.ToString() + "\", \"content\": \"" + content + "\" }" : content;
 	return outp;
     }
-    public Primitive(PrimitiveGroup _primitiveGroup, string _content){
-	primitiveGroup = _primitiveGroup;
+    public Token(TokenGroup _tokenGroup, string _content){
+	tokenGroup = _tokenGroup;
 	content = _content;
     }
 
@@ -194,9 +203,9 @@ public struct Primitive{
 	new string[] {"_"},
     };
     // finds first Primitive in str and removes is from str
-    public static Primitive FindFirstPrimitive(ref string str){
+    public static Token FindFirstPrimitive(ref string str){
 	// setup parameters for no matches
-	PrimitiveGroup pg = PrimitiveGroup.other;
+	TokenGroup pg = TokenGroup.other;
 	string c = str[0].ToString();;
 
 	// find matching PrimitiveGroup
@@ -204,7 +213,7 @@ public struct Primitive{
 	    for(int j = 0; j < primitiveGroupMatches[i].Length; j++){
 		// if matches, set parameters and return
 		if(str.StartsWith(primitiveGroupMatches[i][j])){
-		    pg = (PrimitiveGroup)i;
+		    pg = (TokenGroup)i;
 		    c = primitiveGroupMatches[i][j];
 		    goto _find_first_primitive_return;
 		}
@@ -214,31 +223,58 @@ public struct Primitive{
 _find_first_primitive_return:
 	// remove first Primitive from string and return
 	str = str.Remove(0, c.Length);
-	return new Primitive(pg, c);
+	return new Token(pg, c);
     }
     // finds and returns a list of all Primitives in str
-    public static Primitive[] FindPrimitives(string str){
-	List<Primitive> outp = new List<Primitive>();
+    public static List<Token> FindPrimitives(string str){
+	List<Token> outp = new List<Token>();
 	while(str.Length > 0) outp.Add(FindFirstPrimitive(ref str));
-	return outp.ToArray();
+	return outp;
+    }
+
+    // all combinations of Tokens that can combine into another content
+    // has to be ordered so that special cases come before general ones
+    // before this is used, strings have to be found and made into seperate tokens
+    public static (TokenGroup[] formular, TokenGroup result)[] tokenGroupFormulars = new (TokenGroup[] formular, TokenGroup result)[]{
+	// words
+	(new TokenGroup[] {TokenGroup.word, TokenGroup.letter}, TokenGroup.word),	// words can be extended by letters
+	(new TokenGroup[] {TokenGroup.word, TokenGroup.digit}, TokenGroup.word),	// words can be extended by digits
+	(new TokenGroup[] {TokenGroup.word, TokenGroup.underscore}, TokenGroup.word),	// words can be extended by underscores
+	(new TokenGroup[] {TokenGroup.letter}, TokenGroup.word),			// letters are words
+	(new TokenGroup[] {TokenGroup.underscore}, TokenGroup.word),			// underscores are words
+	// numbers
+	(new TokenGroup[] {TokenGroup.number, TokenGroup.digit}, TokenGroup.number),	// numbers can be extended by digits
+	(new TokenGroup[] {TokenGroup.number, TokenGroup.period}, TokenGroup.number),	// numbers can be extended by periods
+	(new TokenGroup[] {TokenGroup.digit}, TokenGroup.number),			// digits are numbers
+    };
+    // parses all strings in tokens and switches them with a string_ token
+    // returns amount of strings found
+    public static int ParseStrings(ref List<Token> tokens){
+	int stringsFound = 0;
+	while(tokens.Any(t => t.tokenGroup == TokenGroup.stringStarters)){
+	    int starti = tokens.FindIndex(t => t.tokenGroup == TokenGroup.stringStarters);
+	    Token startt = tokens[starti];
+	    int endi = starti;
+	    do{
+		endi = tokens.FindIndex(endi + 1, t => t.tokenGroup == TokenGroup.stringStarters && t.content == startt.content);
+	    }while(IsEscaped(tokens, endi));
+	    stringsFound++;
+	    string c = "";
+	    for(int i = starti; i <= endi; i++) c += tokens[i].content;
+	    Token str = new Token(TokenGroup.string_, c);
+	    tokens.RemoveRange(starti, endi - starti + 1);
+	    tokens.Insert(starti, str);
+	}
+	return stringsFound;
+    }
+    private static bool IsEscaped(List<Token> tokens, int i){
+	bool outp = false;
+	while(--i > 0 && tokens[i].tokenGroup == TokenGroup.escape) outp = !outp;
+	return outp;
     }
 }
-// list of all groups a Primitive can be a part of
-public enum PrimitiveGroup : byte{
-    space		= 0,   // " ", "\t", "\n"
-    letter		= 1,   // alphabetic characters
-    digit		= 2,   // numeric characters
-    period		= 3,   // "."
-    discriminator	= 4,   // ";", ","
-    brackets		= 5,   // normal, square and curly brackets
-    stringStarters	= 6,   // "'''", "\"", "'", other common string starters
-    escape		= 7,   // "\\", other common escape characters
-    underscore		= 8,   // "_"
-    other		= 255, // characters not assigned to another group
-}
-
 // list of all groups a Token can be a part of
-// superset of PrimitiveGroup
+// groups with a value below 128 are considered primitive
 public enum TokenGroup : byte{
     space		= 0,   // " ", "\t", "\n"
     letter		= 1,   // alphabetic characters
@@ -255,127 +291,3 @@ public enum TokenGroup : byte{
     word		= 130, // keyword or variable/function name				(letters + digits? + underscore?)
     other		= 255, // characters not assigned to another group
 }
-
-/*
-public struct Token{
-    public TokenGroup	tokenGroup;
-    public string	content;
-    public int		length;
-
-    public Token(TokenGroup _tokenGroup, string _content){
-	tokenGroup = _tokenGroup;
-	content = _content;
-	length = _content.Length;
-    }
-    public string ToString(bool displayTokenGroup = false){
-	string outp = content;
-	if(displayTokenGroup){
-	    outp = "{\"tokenGroup\": \"" + tokenGroup.ToString() + "\", \"content\": \"" + outp + "\"}";
-	}
-	return outp;
-    }
-
-    // checks top to bottom, index must be the same as value of associated TokenGroup
-    public static (string s, bool splittable)[] tokenGroupDefinitions = new (string s, bool splittable)[] {
-	(" \t\n", false),
-	("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789._", false),
-	("()[]{}<>", true),
-	(",;+-*^/&|!~%", true),
-    };
-    // checks top to bottom
-    public static (string start, string end)[] stringStarters = new (string start, string end)[] {
-	("'''", "'''"),
-	("\"", "\""),
-	("'", "'"),
-	("`", "`"),
-    };
-
-    // returns first token in str
-    public static Token FindFirstToken(ref string str){
-	// throw error on empty string
-	if(str.Length == 0){
-	    Console.WriteLine("Error 2 (I): String in parser is empty");
-	    throw new Exception("Error 2 (I): String in parser is empty");
-	}
-
-	// run string test
-	for(int i = 0; i < stringStarters.Length; i++){
-	    if(str.StartsWith(stringStarters[i].start)){
-		int index = str.IndexOf(stringStarters[i].end);
-
-		// avoid escaped characters (hopefully)
-		while(index < 1 || str[index - 1] == '\\'){
-		    if(index > 0){
-			int ind = index - 1;
-			bool isOdd = false;
-			while(ind > -1 && str[ind--] == '\\') isOdd = !isOdd;
-			if(!isOdd) goto _LoopExit;
-		    }
-		    index = str.IndexOf(stringStarters[i].end, index + 1);
-		}
-_LoopExit:
-
-		// throw error on unclosed string
-		if(index == -1){
-		    Console.WriteLine("Error 1: Found unclosed string");
-		    throw new Exception("Error 1: Found unclosed string");
-		}
-
-		// return string
-		string stringString = str.Substring(0, index + stringStarters[i].end.Length);
-		str = str.Remove(0, stringString.Length);
-		return new Token(TokenGroup.String, stringString);
-	    }
-	}
-
-	// run non-misc group tests
-	for(int i = 0; i < tokenGroupDefinitions.Length; i++){
-	    if(tokenGroupDefinitions[i].s.Contains(str[0])){
-		string tokenGroupToken = str[0].ToString();	// initial value is for splittable case
-
-		// find last instance of unsplittable tokenGroup if tokenGroup is unsplittable
-		if(!tokenGroupDefinitions[i].splittable){
-		    int j = 0;
-		    while(j < str.Length && tokenGroupDefinitions[i].s.Contains(str[j++])) ;
-		    tokenGroupToken = str.Substring(0, j);
-		    if(tokenGroupToken.Length > 1) tokenGroupToken = tokenGroupToken.Remove(tokenGroupToken.Length - 1);
-		}
-
-		// return token
-		str = str.Remove(0, tokenGroupToken.Length);
-		return new Token((TokenGroup)i, tokenGroupToken);
-	    }
-	}
-
-	// return misc token if tokens didnt match
-	string miscToken = str[0].ToString();
-	str = str.Remove(0, 1);
-	return new Token(TokenGroup.Misc, miscToken);
-    }
-    public static Token[] Parse(string str){
-	List<Token> tokens = new List<Token>();
-	while(str.Length > 0){
-	    tokens.Add(FindFirstToken(ref str));
-	}
-	return tokens.ToArray();
-    }
-}
-
-public enum TokenGroup : byte{
-    Space		= 0,
-    Alphanumeric	= 1,
-    Brackets		= 2,
-    DotsNStuff		= 3,
-    Group_4		= 4,
-    Group_5		= 5,
-    Group_6		= 6,
-    Group_7		= 7,
-    Group_8		= 8,
-    Group_9		= 9,
-    Group_10		= 10,
-    Group_11		= 11,
-    Group_12		= 12,
-    Group_13		= 13,
-    String		= 254,	// assumed to be unsplittable
-    Misc		= 255,	// assumed to be splittable
-}*/
