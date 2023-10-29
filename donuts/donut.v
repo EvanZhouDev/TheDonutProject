13cd3039b9c4d7cb4939cacf7145c6991d804392
drakeerv <drakeerv@outlook.com>
2023-10-29 18:28:54 -0400

    Fix spacing issue

--- donuts/donut.v
+++ donuts/donut.v
@@ -12,7 +12,7 @@
  :=  math.sin(i)               for j:=.0 ; j <
  6.28;j+=.02{l:=               math.sin(j)f :=
  math.cos(j)g:=r+             2 q:=1/(l*g*e+w*
- o+5)d:=l*g*o-w*e            m:= int(40+30*q*(
+ o+5)d:=l*g*o-w* e           m:= int(40+30*q*(
   f*g*n-d*c))v:=int(12+15*q*(f*g*c+d*n))x:=m+
    80*v h:=int(8*((w*e-l*r*o)*n-l*r*e-w*o-f*
     r*c))if v<22&& v>=0&& m>=0&& m<79 && q>
